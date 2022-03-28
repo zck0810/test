6b0399dfb71a893308892f6a18d2ee918ad560a3
zck0810 <102041274+zck0810@users.noreply.github.com>
2022-03-29 00:41:21 +0800

    更改游戏
    第一次使用github上传文件

---
+++ 07 猜数字游戏 .c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void menu()
+{
+	printf("*******************************\n");
+	printf("****  1 play      0 exit  *****\n");
+	printf("*******************************\n");
+}
+
+void game()
+{
+	//1、生成一个随机数
+	int guess = 0;
+	int ret = 0;
+	//拿时间戳来设置随机数的生成起始点
+	//time_t time(time_t *timer)
+	ret = rand()%100+1; //生成随机数
+	//printf("%d\n", ret);
+
+	while (1)
+	{
+		printf("请输入猜的数字：");
+		scanf("%d", &guess);
+		if (guess > ret)
+		{
+			printf("猜大了\n");
+		}
+		else if (guess < ret)
+		{
+			printf("猜小了\n");
+		}
+		else
+		{
+			printf("恭喜你，猜对了 \n");
+			break;
+		}
+	}
+}
+
+int main7()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请输入>:");
+		scanf("%d", &input);
+		switch (input)
+		{
+			case 1:
+				game(); //猜数字游戏
+				break;
+			case 0:
+				printf("退出游戏 \n");
+				break;
+			default :
+				printf("选择错误 \n");
+				break;
+		}
+
+	} while (input);
+
+	return 0;
+}
